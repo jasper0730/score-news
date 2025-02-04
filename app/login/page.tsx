078186c1e669dae441ea643c8c8c7d74fc388042
jasper0730 <wilsonlee0730@gmail.com>
2025-02-04 16:55:06 +0800

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const LoginPage = () => {
   return (<div className="flex justify-center w-full px-10 pt-[100px] pb-[50px]">
-    <RegisterClient type={"login"} className="max-w-md w-full" />
+    <RegisterClient type={"login"} className="max-w-md w-full" /> 
     <Link href="/">回首頁</Link>
   </div>);
 }
