1e1b68d379ed73fe3c53a7b396b706a1cca0eee5
Jasper <wilsonlee0730@gmail.com>
2025-06-30 11:13:08 +0800

    test3

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,7 +4,7 @@ import { SyncLoader } from "react-spinners";
 const Loader = () => {
   return (
     <div className="flex items-center justify-center h-[80dvh]">
-      test1
+      test2
       <SyncLoader color="gray" />
     </div>
   )
